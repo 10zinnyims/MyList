5a673b5a3038a94c324b41bd37a6dd17ec7aea50
10zinnyims <tnyima@appdev.yearup.org>
2023-04-12 11:25:41 -0400

    Added new files

---
+++ src/main/java/com/learntocode/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.learntocode;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Egg");
+        System.out.println("Tomatoes");
+        System.out.println("Onion");
+        System.out.println("Milk");
+        System.out.println("Bread");
+        System.out.println("Cheese");
+        System.out.println("Vegetable");
+        System.out.println("Yogurt");
+        System.out.println("Chips");
+        System.out.println("Beans");
+    }
+}
